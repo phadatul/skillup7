dd75d4257f623045d6d3fdb94d7e8ecf0d308673
DIGBIJAYEE-NANDA <digbijayee.nanda07@gmail.com>
2025-03-22 20:01:09 +0530

    From VScode

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello All from Java");
+        System.out.println(new java.util.Date().toString());
+    }
+}
